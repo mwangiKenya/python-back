c708cdfe499d5fbcb808e9c32c80ec09a7c7ab5d
mwangiKenya <mwangiwacollins@gmail.com>
2026-04-05 10:10:09 +0300

    save new changes for readings

--- api/models.py
+++ api/models.py
@@ -12,3 +12,21 @@ class read_users(models.Model):
     class Meta:
         db_table = 'waterusers'
         managed = False
+
+class readings(models.Model):
+    id = models.IntegerField()
+    user_id = models.IntegerField()
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=50)
+    prev_user = models.IntegerField()
+    prev_sup = models.IntegerField()
+    prev_date = models.DateField()
+    cur_user = models.IntegerField()
+    cur_sup = models.IntegerField()
+    cur_date = models.DateField()
+    units_used = models.IntegerField()
+    rate = models.IntegerField()
+
+    class Meta:
+        db_table = 'readings'
+        managed = False
--- api/views.py
+++ api/views.py
@@ -1,5 +1,5 @@
 from django.http import JsonResponse
-from .models import read_users
+from .models import read_users, readings
 
 def water_users(request):
     users = read_users.objects.all()
@@ -13,5 +13,20 @@ def water_users(request):
             'zone' : u.zone,
             'rate' : u.rate,
             'created_on' : u.created_on
+        })
+    return JsonResponse(data, safe=False)
+
+def read_data(request):
+    read = readings.objects.all()
+    data = []
+    for r in read:
+        data.append({
+            'name' : r.name,
+            'phone' : r.phone,
+            'prev_user' : r.prev_user,
+            'prev_sup' : r.prev_sup,
+            'cur_user' : r.cur_user,
+            'cur_sup' : r.cur_sup,
+            'rate' : r.rate
         })
     return JsonResponse(data, safe=False)
