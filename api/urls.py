4c9cfd3a649134e55be0c0d42792988a15ca87ee
mwangiKenya <mwangiwacollins@gmail.com>
2026-05-14 21:42:12 +0300

    Save changes

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import  water_users, read_data, login_user, new_user,update_user, submit_new_reading, bill, logs, update_paid, total_bill, register_user, list_employees, total_paid, avg_units, total_cust, total_units, users_login, delete_user, delete_employee, update_employee, download_readings_template, upload_readings_excel, hist_data, send_sms_view, download_billings_template, upload_billings_excel, reset_mid_month_readings, billing_timer, finalize_month, restore_readings, start_billing_month
+from .views import  water_users, read_data, login_user, new_user,update_user, submit_new_reading, bill, logs, update_paid, total_bill, register_user, list_employees, total_paid, avg_units, total_cust, total_units, users_login, delete_user, delete_employee, update_employee, download_readings_template, upload_readings_excel, hist_data, send_sms_view, download_billings_template, upload_billings_excel, reset_mid_month_readings, billing_timer, finalize_month, restore_readings, start_billing_month, cycle_timer_status, set_cycle_duration
 
 urlpatterns =[
     path('water_users/', water_users, name='water_users'), #fetch water users data
@@ -37,4 +37,6 @@ urlpatterns =[
     path("finalize_month/", finalize_month, name="finalize_month"),
     path("restore_readings/", restore_readings, name="restore_readings"),
     path("start_billing_month/", start_billing_month),
+    path("set_cycle_duration/", set_cycle_duration),
+    path("cycle_timer_status/", cycle_timer_status),
 ]
