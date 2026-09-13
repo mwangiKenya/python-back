ec85993571fe98ca7e10fa7ae3f2f7b2da178604
mwangiKenya <mwangiwacollins@gmail.com>
2026-09-13 13:48:47 +0300

    Add changes on billings

--- api/views.py
+++ api/views.py
@@ -1431,72 +1431,52 @@ def download_billings_template(request):
         "templates",
         "Billings.xlsx"
     )
-
     # Load the existing Excel template
     wb = load_workbook(template_path)
     ws = wb.active
-
     # Get billing records from the database
     billings_data = Billings.objects.all().order_by("id")
-
     # Start inserting customer data from row 6
     row = 6
     #CALCULATE THE REQUIRED METRICS
     total_customers = Billings.objects.count()
-
-
     for billing in billings_data:
         #total customers
         ws.cell(row=2, column=9).value=total_customers
         ws.cell(row=2, column=3).value='Aug-2026'
         ws.cell(row=2, column=6).value='31-Aug-26'
-
         # A - ID
         ws.cell(row=row, column=1).value = billing.id
-
         # B - Name
         ws.cell(row=row, column=2).value = billing.name
-
         # C - Phone
         ws.cell(row=row, column=3).value = billing.phone
-
         # D - Units Used
         ws.cell(row=row, column=4).value = billing.units_used
-
         # E - Rate
         ws.cell(row=row, column=5).value = billing.rate
-
         # F - Bill
         ws.cell(row=row, column=6).value = billing.bill
-
         # G - B_CD / Previous Balance
         ws.cell(row=row, column=7).value = billing.b_cd
-
         # H - Penalty
         ws.cell(row=row, column=8).value = billing.penalty
-
         # I - Bill + B_CD + Penalty
         bill = billing.bill or Decimal("0")
         b_cd = billing.b_cd or Decimal("0")
         penalty = billing.penalty or Decimal("0")
-
         ws.cell(row=row, column=9).value = bill + b_cd + penalty
-
         # J - Amount Paid
         # Leave this blank for the user to enter payment
         ws.cell(row=row, column=10).value = None
-
         # K - Balance
         # I - J
         ws.cell(row=row, column=11).value = f"=I{row}-J{row}"
-
         row += 1
-
     # Tell Excel to recalculate formulas when the file is opened
     wb.calculation.fullCalcOnLoad = True
     wb.calculation.forceFullCalc = True
     wb.calculation.calcMode = "auto"
-
     # Create Excel response
     response = HttpResponse(
         content_type=(
@@ -1504,14 +1484,11 @@ def download_billings_template(request):
             "spreadsheetml.sheet"
         )
     )
-
     response["Content-Disposition"] = (
         'attachment; filename="billings_template.xlsx"'
     )
-
     # Save workbook directly to response
     wb.save(response)
-
     return response
 @csrf_exempt
 def upload_billings_excel(request):
